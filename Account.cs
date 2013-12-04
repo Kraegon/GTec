d432c1d48629de0e76d29cdba5b72db1fcd61aef
Kraegon <Julio34@gmail.com>
2013-12-04 12:45:00 +0100

    Initial commit. Contains the basic model that will be the lifeline of te application

---
+++ Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Account
+    {
+        public string gebruikersnaam;
+        public string password;
+
+        public Account(string gebruikersnaam, string password)
+        {
+            this.gebruikersnaam = gebruikersnaam;
+            this.password = password;
+        }
+    }
+}
---
+++ Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Route
+    {
+        public string Name;
+        public string SystemSoundPath;
+        public WayPoint[] wayPoints;
+
+        public Route(string Name, string SystemSoundPath, WayPoint[] wayPoints)
+        {
+            this.Name = Name;
+            this.SystemSoundPath = SystemSoundPath;
+            this.wayPoints = wayPoints;
+        }
+    }
+}
