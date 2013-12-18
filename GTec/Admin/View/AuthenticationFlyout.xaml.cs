32e715de0d79d07e1e34fe36685a64ba6c653b6b
Lesley van Hoek <lesleyvanhoek@hotmail.com>
2013-12-18 13:13:13 +0100

    Controller has GetInstance(), Login functionality

--- GTec/Admin/View/AuthenticationFlyout.xaml.cs
+++ GTec/Admin/View/AuthenticationFlyout.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,10 +25,21 @@ namespace GTec.User.View
             this.InitializeComponent();
         }
 
-        private void OKLoginButton_Click(object sender, RoutedEventArgs e)
+        private async void OKLoginButton_Click(object sender, RoutedEventArgs e)
         {
             Tuple<string, string> authentication = new Tuple<string, string>(UsernameText.Text, PasswordText.Password);
-            //Check for accountzz and give user access to database
+            List<Model.Account> accounts = await User.Controller.DatabaseConnector.INSTANCE.GetAccountsAsync();
+            foreach (User.Model.Account a in accounts)
+            {
+                if (a.Gebruikersnaam.Equals(authentication.Item1) && a.Password.Equals(authentication.Item2))
+                {
+                    User.Controller.Control.SetCurrentAccount(a);
+                    await new MessageDialog("Succesfully logged in as " + a.Gebruikersnaam + "!").ShowAsync();
+                    this.Hide();
+                    return;
+                }
+            }
+            MessageTextBlock.Text = "Incorrect credentials!";
         }
     }
 }
